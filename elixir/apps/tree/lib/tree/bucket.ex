1a657d1dfb107eb04b97d2c963351813611f83fa
Andreas Altendorfer <andreas@altendorfer.at>
2017-08-28 11:58:36 +0200

    Add Bucket.add and .get

--- elixir/apps/tree/lib/tree/bucket.ex
+++ elixir/apps/tree/lib/tree/bucket.ex
@@ -7,4 +7,20 @@ defmodule Tree.Bucket do
     Agent.start_link( fn() -> %{} end)
   end
 
+  def get(pid) do
+    Agent.get(pid, fn(bucket) -> bucket end)
+  end
+
+  def get(pid, key) do
+    Agent.get(pid, fn(bucket) ->
+      Map.get(bucket, key)
+    end)
+  end
+
+  def add(pid, key, value) do
+    Agent.update(pid, fn(bucket) ->
+      Map.put(bucket, key, value)
+    end)
+  end
+
 end
