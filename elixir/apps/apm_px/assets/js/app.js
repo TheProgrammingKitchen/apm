c623a74067e4a12482a9daa27a1bb516cf2bd512
Andreas Altendorfer <andreas@altendorfer.at>
2017-09-08 18:38:06 +0200

    Enhance UX

--- elixir/apps/apm_px/assets/js/app.js
+++ elixir/apps/apm_px/assets/js/app.js
@@ -12,10 +12,14 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-
+import $ from "jquery"
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+$("#show-debug").click(function(event) {
+  event.preventDefault();
+  $(".debug").toggle();
+});
