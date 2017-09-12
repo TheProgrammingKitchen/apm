aac451c8f494283ec1e7ee42496cad3bd754578c
Andreas Altendorfer <andreas@altendorfer.at>
2017-09-12 16:41:24 +0200

    Shift debuglevel to info in development

--- elixir/apps/apm_px/config/dev.exs
+++ elixir/apps/apm_px/config/dev.exs
@@ -27,7 +27,8 @@ config :apm_px, ApmPx.Web.Endpoint,
   ]
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, format: "[$level] $message\n",
+ level: :info
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
